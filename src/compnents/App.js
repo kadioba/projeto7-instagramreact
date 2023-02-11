2a73093952ba4c6ff550cc28f0303fa8e0c32fc1
Kadioba <kadiobaoff@gmail.com>
2023-02-11 16:15:58 -0300

    Versao com layout modularizado em partes basicas

---
+++ src/compnents/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Corpo from "./Corpo";
+import NavBar from "./NavBar";
+
+export default function App(){
+    return(
+        <>
+            <NavBar />
+            <Corpo />
+        </>
+
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from "react-dom";
+import App from "./compnents/App";
+
+ReactDOM.render(<App />, document.querySelector(".root"))
